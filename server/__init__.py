1c34012ba176e4122949f533e28f4ace816ac362
russell little <rrlittle2@gmail.com>
2020-12-13 18:52:01 -0800

    must bind to

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,16 @@
 from flask_socketio import SocketIO
 from flask import Flask
 from flask_cors import CORS
+from os import environ
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 CORS(app)
 app.config['SECRET_KEY'] = 'secret'
 app.debug = True
-socketio = SocketIO(app, cors_allowed_origins="*", logger=True)
+socketio = SocketIO(app,
+                    port=environ.get('PORT', 5000),
+                    cors_allowed_origins="*",
+                    logger=True)
 
 from . import events
 from . import rest
