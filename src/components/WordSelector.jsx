b288c19ac720665435af68a45cc7f8d70ecdf101
russell little <rrlittle2@gmail.com>
2020-12-15 16:27:59 -0800

    improved the word selector

--- src/components/WordSelector.jsx
+++ src/components/WordSelector.jsx
@@ -2,27 +2,64 @@ import React, { useState, useEffect } from "react";
 import { Segment, Header, Button, Item } from "semantic-ui-react";
 
 const WordSelector = () => {
-    const [res, setRes] = useState({});
-    const fetchWord = () =>
+    const [loading, setLoading] = useState(false);
+    const [res, setRes] = useState([]);
+    const fetchWord = () => {
+        setLoading(true);
         fetch("/word")
             .then((resp) => resp.json())
-            .then(setRes);
+            .then(({ word, definition }) => {
+                fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+                    .then((resp) => {
+                        if (resp.status === 200) return resp.json();
+                        throw new Error();
+                    })
+                    .then((defs) => {
+                        setRes(defs);
+                        setLoading(false);
+                    })
+                    .catch(() => {
+                        setRes([
+                            {
+                                word,
+                                meanings: [{ definitions: [{ definition }] }],
+                            },
+                        ]);
+                    });
+            });
+    };
     useEffect(() => {
         fetchWord();
     }, []);
-    console.log("fetchWord", res);
-    const word = res.word;
-    const def = res.definition;
     return (
         <Segment>
-            <Header content="Random Word" />
             <Item.Group>
-                <Item>
-                    <Item.Content>
-                        <Item.Header as="a">{word}</Item.Header>
-                        <Item.Description>{def}</Item.Description>
-                    </Item.Content>
-                </Item>
+                {res.map(({ word, phonetics = [], meanings = [] }, i) => (
+                    <Item key={i}>
+                        <Item.Content>
+                            <Item.Header>{word}</Item.Header>
+                            {meanings.map(
+                                ({ partOfSpeech, definitions = [] }, j) =>
+                                    definitions.map(
+                                        ({ definition, example }) => (
+                                            <React.Fragment key={j}>
+                                                <Item.Description>
+                                                    <i>{partOfSpeech}</i>{" "}
+                                                    {definition}
+                                                </Item.Description>
+                                                <Item.Extra>
+                                                    {example}
+                                                </Item.Extra>
+                                            </React.Fragment>
+                                        )
+                                    )
+                            )}
+                            {phonetics.map(({ text }, j) => (
+                                <Item.Meta key={j}>{text}</Item.Meta>
+                            ))}
+                        </Item.Content>
+                    </Item>
+                ))}
             </Item.Group>
             <Button content="get a random word" onClick={fetchWord} />
         </Segment>
