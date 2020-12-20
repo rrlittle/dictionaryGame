3fe47d56f0397caeb3af55efa1d6ce436ef565c6
russell little <rrlittle2@gmail.com>
2020-12-20 15:09:46 -0800

    pass Menu.Item props to MenuItem's childrent o highlight stuff

--- src/components/screens/MenuItem.jsx
+++ src/components/screens/MenuItem.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 import "./style/MenuItem.css";
 
-const MenuItem = ({ icon, content }) => (
+const MenuItem = ({ icon, content, ...props }) => (
     <Menu.Item
+        {...props}
         icon={icon}
         content={<div className="hideOnMobile">{content}</div>}
     />
